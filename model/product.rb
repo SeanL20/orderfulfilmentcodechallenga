9f832840acd67608eefcbd80edce39acb7c4ee6d
Sean Lim <seanl30@yahoo.com>
2022-09-08 16:09:50 +1000

    created product class

---
+++ model/product.rb
@@ -0,0 +1,17 @@
+class Product
+    attr_accessor :product_id, 
+        :description,
+        :quantity_on_hand, 
+        :reorder_threshold, 
+        :reorder_amount, 
+        :delivery_lead_time
+
+    # Initialize a product class
+	def initialize(product_id:, description:, quantity_on_hand:, reorder_threshold:, delivery_lead_time:)
+		@product_id = product_id
+		@description = description.to_i
+		@quantity_on_hand = quantity_on_hand.to_i
+		@reorder_threshold = reorder_threshold.to_i
+		@delivery_lead_time = delivery_lead_time.to_i
+	end
+end
