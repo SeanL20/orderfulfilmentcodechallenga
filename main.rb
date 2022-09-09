4af1055c9f9b2a877355842a5d6655cf60b1edf5
Sean Lim <seanl30@yahoo.com>
2022-09-09 16:59:03 +1000

    minor change

--- main.rb
+++ main.rb
@@ -1,6 +1,12 @@
+require_relative 'model/commands'
+
 def main
     input_file = ARGV[0]
 
+    product = Product.new
+
+    Commands.store_product_data(input_file, product)
+    puts product.inspect
 end
 
 main
