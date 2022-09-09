1a39712e7b1b2cfa27a5086b6cb3544e6c80ea77
Sean Lim <seanl30@yahoo.com>
2022-09-09 21:52:50 +1000

    Added the method to find the product item

--- model/product.rb
+++ model/product.rb
@@ -7,4 +7,10 @@ class Product
 	def initialize()
 		@product_items = []
 	end
+
+    def find_product_item_by_id(id)
+        @product_items.each do |product_item|
+            return product_item if product_item.product_id == id
+        end
+    end
 end
