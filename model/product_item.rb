4b2ff0e3afcde1eb8515b75779bb36ce3b20a494
Sean Lim <seanl30@yahoo.com>
2022-09-09 16:58:08 +1000

    minor changes

--- model/product_item.rb
+++ model/product_item.rb
@@ -8,11 +8,12 @@ class ProductItem
         :product
 
     # Initialize a product class
-	def initialize(product_id:, description:, quantity_on_hand:, reorder_threshold:, delivery_lead_time:, product:)
+	def initialize(product_id:, description:, quantity_on_hand:, reorder_threshold:, reorder_amount:, delivery_lead_time:, product:)
 		@product_id = product_id
 		@description = description.to_i
 		@quantity_on_hand = quantity_on_hand.to_i
 		@reorder_threshold = reorder_threshold.to_i
+		@reorder_amount = reorder_amount.to_i
 		@delivery_lead_time = delivery_lead_time.to_i
         @product = product
 
